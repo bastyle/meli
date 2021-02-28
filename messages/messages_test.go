95cf7785be7363902a32bccdd05714fc18fd7cbd
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-02-27 22:35:55 -0300

    add test

---
+++ messages/messages_test.go
@@ -0,0 +1,21 @@
+package messages
+
+import (
+	"testing"
+)
+
+var kenobi_msg = [5]string{"este", "", "", "mensaje", ""}
+var skywalker_msg = [5]string{"", "es", "", "", "secreto"}
+var sato_msg = [5]string{"este", "", "un", "", ""}
+var kenobi_msg_6 = [6]string{"este", "", "", "mensaje", "", ""}
+
+func TestMsgLen(t *testing.T) {
+	if valid, err := ValidateMessagesLen(kenobi_msg_6[:], skywalker_msg[:], sato_msg[:]); err == nil {
+		t.Errorf("Expected: %v, got: %v", false, valid)
+		t.Errorf("Se esperaba ser invalido debido al largo de uno de los mensajes.")
+	}
+	if valid, err := ValidateMessagesLen(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); err != nil {
+		t.Errorf("Expected: %v, got: %v", true, valid)
+		t.Errorf("Se esperaba que todos los mensajes tuviera la misma cantidad de palabras.")
+	}
+}
