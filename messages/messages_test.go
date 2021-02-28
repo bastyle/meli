c37a9384b16f4549904d989776e346f7ac4db28d
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-02-28 12:35:39 -0300

    test logs

--- messages/messages_test.go
+++ messages/messages_test.go
@@ -11,19 +11,23 @@ var kenobi_msg_6 = [6]string{"este", "", "", "mensaje", "", ""}
 var expectedMsg = "este es un mensaje secreto"
 
 func TestMsgLen(t *testing.T) {
-	//t.Log("TestMsgLen...")
 	if valid, err := ValidateMessagesLen(kenobi_msg_6[:], skywalker_msg[:], sato_msg[:]); err == nil {
 		t.Errorf("Expected: %v, got: %v", false, valid)
 		t.Errorf("Se esperaba ser invalido debido al largo de uno de los mensajes.")
+	} else {
+		t.Log("TestMsgLen PASSED")
 	}
-	if valid, err := ValidateMessagesLen(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); err != nil {
-		t.Errorf("Expected: %v, got: %v", true, valid)
-		t.Errorf("Se esperaba que todos los mensajes tuviera la misma cantidad de palabras.")
+	if _, err := ValidateMessagesLen(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); err != nil {
+		t.Errorf("TestMsgLen FAILED: All messages were expected to have the same number of words.")
+	} else {
+		t.Log("TestMsgLen 2 PASSED")
 	}
 }
 
 func TestExpectedMsg(t *testing.T) {
 	if msg := GetMessage(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); msg != expectedMsg {
-		t.Errorf("Expected: %v, got: %v", expectedMsg, msg)
+		t.Errorf("FAILED: Expected: %v, got: %v", expectedMsg, msg)
+	} else {
+		t.Log("TestExpectedMsg PASSED")
 	}
 }
