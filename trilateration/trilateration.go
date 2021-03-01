c10681537aafb1d49c3eb979938fa05ac1f04f67
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-02-28 22:43:02 -0300

    tri test

--- trilateration/trilateration.go
+++ trilateration/trilateration.go
@@ -5,13 +5,6 @@ import (
 	"math"
 )
 
-/*type Coordinate struct {
-	x    float64
-	y    float64
-	d    float64
-	uuid string
-}*/
-
 type Satellite struct {
 	x    float64
 	y    float64
@@ -19,7 +12,7 @@ type Satellite struct {
 	name string
 }
 
-//agregar valores de inicio a archivo properties posiblemente modicable por un mantenedor
+//TODO agregar valores de inicio a archivo de propiedades posiblemente modicable por un mantenedor
 var kenobiSat = Satellite{
 	x: -500,
 	y: -200,
@@ -44,44 +37,6 @@ func Hello(name string) string {
 	return message
 }
 
-/*func (c1 Coordinate) Trilaterate(c2, c3 Coordinate) (x, y float64) {
-	return Trilaterate(c1, c2, c3)
-}
-
-func Trilaterate(c1, c2, c3 Coordinate) (x, y float64) {
-	d1, d2, d3, i1, i2, i3, j1, j2, j3 := &c1.d, &c2.d, &c3.d, &c1.x, &c2.x, &c3.x, &c1.y, &c2.y, &c3.y
-	x = ((((math.Pow(*d1, 2)-math.Pow(*d2, 2))+(math.Pow(*i2, 2)-math.Pow(*i1, 2))+(math.Pow(*j2, 2)-math.Pow(*j1, 2)))*(2**j3-2**j2) - ((math.Pow(*d2, 2)-math.Pow(*d3, 2))+(math.Pow(*i3, 2)-math.Pow(*i2, 2))+(math.Pow(*j3, 2)-math.Pow(*j2, 2)))*(2**j2-2**j1)) / ((2**i2-2**i3)*(2**j2-2**j1) - (2**i1-2**i2)*(2**j3-2**j2)))
-	y = ((math.Pow(*d1, 2) - math.Pow(*d2, 2)) + (math.Pow(*i2, 2) - math.Pow(*i1, 2)) + (math.Pow(*j2, 2) - math.Pow(*j1, 2)) + x*(2**i1-2**i2)) / (2**j2 - 2**j1)
-	return x, y
-}
-
-func GetLocation(distances ...float32) (x, y float32) {
-	fmt.Println("GetLocation distances: ", distances)
-	var kenobi = Coordinate{
-		x: -3.0,
-		y: -2.0,
-		//d: 7.21,
-		d: float64(distances[0]),
-	}
-	var skywalker = Coordinate{
-		x: 2.0,
-		y: -2.0,
-		//d: 6.08,
-		d: float64(distances[1]),
-	}
-	var sato = Coordinate{
-		x: 3.0,
-		y: 3.0,
-		//d: 2.23,
-		d: float64(distances[2]),
-	}
-	xFromKenobi, yFromKenobi := kenobi.Trilaterate(skywalker, sato)
-	xFromSkywalker, yFromSkywalker := skywalker.Trilaterate(sato, kenobi)
-	xFromSato, yFromSato := sato.Trilaterate(skywalker, kenobi)
-
-	return GetAvgCoorValue(xFromKenobi, xFromSato, xFromSkywalker), GetAvgCoorValue(yFromKenobi, yFromSato, yFromSkywalker)
-}*/
-
 func GetAvgCoorValue(coordinateValues ...float64) float32 {
 	var finalCoor float32
 	var coorSum float64
@@ -97,27 +52,27 @@ func GetAvgCoorValue(coordinateValues ...float64) float32 {
 }
 
 // con estructura satelites
-func (c1 Satellite) Trilaterate2(c2, c3 Satellite) (x, y float64) {
-	return Trilaterate2(c1, c2, c3)
+func (c1 Satellite) Trilaterate(c2, c3 Satellite) (x, y float64) {
+	return Trilaterate(c1, c2, c3)
 }
 
-func Trilaterate2(c1, c2, c3 Satellite) (x, y float64) {
+func Trilaterate(c1, c2, c3 Satellite) (x, y float64) {
 	d1, d2, d3, i1, i2, i3, j1, j2, j3 := &c1.d, &c2.d, &c3.d, &c1.x, &c2.x, &c3.x, &c1.y, &c2.y, &c3.y
 	x = ((((math.Pow(*d1, 2)-math.Pow(*d2, 2))+(math.Pow(*i2, 2)-math.Pow(*i1, 2))+(math.Pow(*j2, 2)-math.Pow(*j1, 2)))*(2**j3-2**j2) - ((math.Pow(*d2, 2)-math.Pow(*d3, 2))+(math.Pow(*i3, 2)-math.Pow(*i2, 2))+(math.Pow(*j3, 2)-math.Pow(*j2, 2)))*(2**j2-2**j1)) / ((2**i2-2**i3)*(2**j2-2**j1) - (2**i1-2**i2)*(2**j3-2**j2)))
 	y = ((math.Pow(*d1, 2) - math.Pow(*d2, 2)) + (math.Pow(*i2, 2) - math.Pow(*i1, 2)) + (math.Pow(*j2, 2) - math.Pow(*j1, 2)) + x*(2**i1-2**i2)) / (2**j2 - 2**j1)
 	return x, y
 }
 
-func GetLocation2(distances ...float32) (x, y float32) {
+func GetLocation(distances ...float32) (x, y float32) {
 	fmt.Println("GetLocation distances: ", distances)
 
 	kenobiSat.d = float64(distances[0])
 	skywalkerSat.d = float64(distances[1])
 	satoSat.d = float64(distances[2])
 
-	xFromKenobi, yFromKenobi := kenobiSat.Trilaterate2(skywalkerSat, satoSat)
-	xFromSkywalker, yFromSkywalker := skywalkerSat.Trilaterate2(satoSat, kenobiSat)
-	xFromSato, yFromSato := satoSat.Trilaterate2(skywalkerSat, kenobiSat)
+	xFromKenobi, yFromKenobi := kenobiSat.Trilaterate(skywalkerSat, satoSat)
+	xFromSkywalker, yFromSkywalker := skywalkerSat.Trilaterate(satoSat, kenobiSat)
+	xFromSato, yFromSato := satoSat.Trilaterate(skywalkerSat, kenobiSat)
 
 	return GetAvgCoorValue(xFromKenobi, xFromSato, xFromSkywalker), GetAvgCoorValue(yFromKenobi, yFromSato, yFromSkywalker)
 }
--- trilateration/trilateration_test.go
+++ trilateration/trilateration_test.go
@@ -8,43 +8,10 @@ import (
 	"testing"
 )
 
-/*var a = Coordinate{
-	x: -3,
-	y: -2,
-	d: 7.21,
-}
-var b = Coordinate{
-	x: 2,
-	y: -2,
-	d: 6.08,
-}
-var c = Coordinate{
-	x: 3,
-	y: 3,
-	d: 2.23,
-}
-
-func TestTrilateration(t *testing.T) {
-	fmt.Println("TestTrilateration")
-	x, y := a.Trilaterate(b, c)
-	fmt.Println("Coordinates of intersections center using formula x:", x, "y:", y)
-	x1, y1 := b.Trilaterate(c, a)
-	fmt.Println("b->(c,a) x:", x1, "y:", y1)
-	x2, y2 := c.Trilaterate(b, a)
-	fmt.Println("c->(b,a) x:", x2, "y:", y2)
-}
-
 func TestGetLocation(t *testing.T) {
 	fmt.Println("TestGetLocation")
-	x, y := GetLocation(7.21, 6.08, 2.23)
-	//fmt.Println(GetLocation(7.21, 6.08, 2.23))
-	fmt.Printf("x: %.1f y: %.1f \n", x, y)
-}
-*/
-func TestGetLocation2(t *testing.T) {
-	fmt.Println("TestGetLocation2")
 	//ejemplo graficamente real para punto D -100,400
-	x, y := GetLocation2(721.11, 538.51, 670.82)
+	x, y := GetLocation(721.11, 538.51, 670.82)
 	fmt.Printf("x: %.1f y: %.1f \n", x, y)
 	if auxX, _ := strconv.ParseFloat(fmt.Sprintf("%.1f", x), 32); auxX != -100 {
 		t.Errorf("TestGetLocation FAILED: Expected x = -100 && y = 400 and got x=%.2f", auxX)
@@ -59,6 +26,12 @@ func TestGetLocation2(t *testing.T) {
 
 }
 
+func TestAvgCoorValue(t *testing.T) {
+	if x := GetAvgCoorValue(1.0017699999999998, 3.9989959999999996, math.NaN()); x != float32(2.500383) {
+		t.Errorf("TestAvgCoorValue FAILED: Expected x = 2.5 and got x=%f", x)
+	}
+}
+
 /*******challa********/
 
 func TestType(t *testing.T) {
@@ -68,12 +41,6 @@ func TestType(t *testing.T) {
 	} else {
 		fmt.Println("not nan")
 	}
-}
-
-func TestReadCoorValues(t *testing.T) {
-	x := GetAvgCoorValue(1.0017699999999998, 3.9989959999999996, math.NaN())
-	fmt.Println(fmt.Sprintf("%.2f", x))
-
 }
 
 func TestHello(t *testing.T) {
