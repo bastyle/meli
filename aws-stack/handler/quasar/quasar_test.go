282b83a992e2d9f701d88ff58b965caee89dc263
Bastian Bastias <bastian.bastias@consorcio.cl>
2021-03-02 12:52:08 -0300

    fix test

--- aws-stack/handler/quasar/quasar_test.go
+++ aws-stack/handler/quasar/quasar_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestMsg(t *testing.T) {
-	if msg := getMessage(); msg != nil {
+	if msg := getMessage(); (Message{}) != msg {
 		t.Log("TestExpectedMsg PASSED")
 	} else {
 		t.Error("FAILED.")
