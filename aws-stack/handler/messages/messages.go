9d92cd28f7ccb79784230fe85b71f778b9e3eaf0
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-01 21:42:18 -0300

    add message func proof

--- aws-stack/handler/messages/messages.go
+++ aws-stack/handler/messages/messages.go
@@ -51,9 +51,13 @@ type Message struct {
 	age  int
 }
 
+type Response struct {
+	message string
+}
+
 func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//body, err := json.Marshal("{'list':'lista'}")
-	body, err := json.Marshal(getMessage())
+	//body, err := json.Marshal(getMessage())
+	body, err := json.Marshal(getMessage2())
 	if err != nil {
 		return events.APIGatewayProxyResponse{Body: "Unable to marshal JSON", StatusCode: 500}, nil
 	}
@@ -62,6 +66,13 @@ func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 
 func getMessage() Message {
 	return Message{"Alice", "Hello", 37}
+}
+
+func getMessage2() Response {
+	var kenobi_msg = [5]string{"este", "", "", "mensaje", ""}
+	var skywalker_msg = [5]string{"", "es", "", "", "secreto"}
+	var sato_msg = [5]string{"este", "", "un", "", ""}
+	return Response{GetMessage(kenobi_msg[:], skywalker_msg[:], sato_msg[:])}
 }
 
 func main() {
