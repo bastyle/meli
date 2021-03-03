f50d55fd6986ed0a7a1f74997a5c26087c869e00
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-03 10:46:42 -0300

    json req

--- aws-stack/handler/quasar/quasar.go
+++ aws-stack/handler/quasar/quasar.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 
@@ -52,19 +51,50 @@ type RequestBody struct {
 	return events.APIGatewayProxyResponse{Body: string(jsonBody), StatusCode: 200}, nil
 }*/
 
-func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("request.Body = %v.\n", req.Body)
+	fmt.Printf("request.Body size = %d.\n", len(req.Body))
+	//reqBodyStruct := RequestBody{}
+	reqBodyStruct := new(RequestBody)
+	err := json.Unmarshal([]byte(req.Body), reqBodyStruct)
+	//acá
+	if err != nil {
+		fmt.Printf("Error unMarshal req.Body= %v.\n", err)
+		return events.APIGatewayProxyResponse{Body: "Unable to unmarshal JSON req.body ", StatusCode: 500}, nil
+	} else {
+		fmt.Printf("reqBodyStruct.Satellites[0].Name = %v.\n", reqBodyStruct.Satellites[0].Name)
+		reqBodyStruct.Satellites[0].Name = "Prueba"
+		fmt.Printf("reqBodyStruct.Satellites[0].Name = %v.\n", reqBodyStruct.Satellites[0].Name)
+	}
+	if jsonResBody, err := json.Marshal(reqBodyStruct); err != nil {
+		fmt.Printf("Error marshal reqBodyStruct= %v.\n", err)
+		return events.APIGatewayProxyResponse{Body: "Error transformando response body a objeto JSON", StatusCode: 500}, nil
+	} else {
+		fmt.Printf("transformacion de response ok %v: .\n", err)
+		return events.APIGatewayProxyResponse{Body: string(jsonResBody), StatusCode: 200}, nil
+	}
+}
+
+/*func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
 	fmt.Printf("Body size = %d.\n", len(request.Body))
+	//agregar trim
 	jsonBody, err := json.Marshal(request.Body)
 	if err != nil {
 		return events.APIGatewayProxyResponse{Body: "Unable to marshal JSON", StatusCode: 500}, nil
 	} else {
 		fmt.Printf("reqJsonBody: %v.\n", string(jsonBody))
 	}
-	/*fmt.Println("Headers:")
+	fmt.Println("Headers:")
 	for key, value := range request.Headers {
 		fmt.Printf("    %s: %s\n", key, value)
-	}*/
+	}
+	if last := len(string(jsonBody)) - 1; last >= 0 && string(jsonBody)[last] == '"' {
+		jsonBody = jsonBody[:last]
+	}
+	if len(body) >= 0 && body[0] == '"' {
+		body = body[1:]
+	}
 	reqBodyStruct := RequestBody{}
 	err1 := json.Unmarshal([]byte(jsonBody), &reqBodyStruct)
 	if err1 != nil {
@@ -77,11 +107,8 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	} else {
 		return events.APIGatewayProxyResponse{Body: string(jsonResBody), StatusCode: 200}, nil
 	}
-
-	//return events.APIGatewayProxyResponse{Body: jsonBody, StatusCode: 200}, nil
-
 	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
-}
+}*/
 
 func GetExampleMessage() MockResponse {
 	var kenobi_msg = [5]string{"este", "", "", "mensaje", ""}
--- aws-stack/handler/quasar/quasar_test.go
+++ aws-stack/handler/quasar/quasar_test.go
@@ -22,8 +22,16 @@ func TestExampleMessage(t *testing.T) {
 
 func TestJsonRequest(t *testing.T) {
 	t.Log("TestJsonRequest ...")
-	body := `{"satellites":[{"name":"kenobi","distance":100.0,"message":["este","","","mensaje",""]},{"name":"skywalker","distance":115.5,"message":["","es","","","secreto"]},{"name":"sato","distance":142.7,"message":["este","","un","",""]}]}`
-	//body := `"{"satellites":[{"name":"kenobi","distance":100.0,"message":["este","","","mensaje",""]},{"name":"skywalker","distance":115.5,"message":["","es","","","secreto"]},{"name":"sato","distance":142.7,"message":["este","","un","",""]}]}"`
+	//body := `{"satellites":[{"name":"kenobi","distance":100.0,"message":["este","","","mensaje",""]},{"name":"skywalker","distance":115.5,"message":["","es","","","secreto"]},{"name":"sato","distance":142.7,"message":["este","","un","",""]}]}`
+	body := `"{"satellites":[{"name":"kenobi","distance":100.0,"message":["este","","","mensaje",""]},{"name":"skywalker","distance":115.5,"message":["","es","","","secreto"]},{"name":"sato","distance":142.7,"message":["este","","un","",""]}]}"`
+
+	if last := len(body) - 1; last >= 0 && body[last] == '"' {
+		body = body[:last]
+	}
+	if len(body) >= 0 && body[0] == '"' {
+		body = body[1:]
+	}
+
 	t.Log("TestJsonRequest body: ", body)
 	jsonBody, err := json.Marshal(body)
 	if err != nil {
@@ -41,3 +49,22 @@ func TestJsonRequest(t *testing.T) {
 	//fmt.Printf("\n\n json object:::: %+v", bodyJSON.satellites[0].Name)
 	fmt.Printf("\n\n json object:::: %+v", bodyJSON.Satellites[0].Name)
 }
+
+func TestByteStr(t *testing.T) {
+	b := []byte{34, 65, 66, 67, 226, 130, 172, 34}
+	fmt.Printf("b: %v \n", b)
+	s := string(b)
+	if b[0] == 34 {
+		fmt.Printf("quotes: %v \n", b[0])
+		b = b[1 : len(b)-1]
+	}
+	fmt.Printf("b: %v \n", b)
+	fmt.Printf("str: %v \n", s)
+}
+
+/*func TrimSuffix(s, suffix string) string {
+    if strings.HasSuffix(s, suffix) {
+        s = s[:len(s)-len(suffix)]
+    }
+    return s
+}*/
