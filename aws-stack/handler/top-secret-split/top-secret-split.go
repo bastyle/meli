bcc5b21f575c76f754689aa8adc99d7126b34906
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-05 14:34:04 -0300

    path params

--- aws-stack/handler/top-secret-split/top-secret-split.go
+++ aws-stack/handler/top-secret-split/top-secret-split.go
@@ -9,16 +9,17 @@ import (
 
 func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	name := "Bastian"
-	fmt.Printf("hello: %s", name)
+	fmt.Printf("hello: %s.\n", name)
 	//greet, _ := GetMessages(name)
-	fmt.Printf("router = %v.\n", req)
+	fmt.Printf("req = %v.\n", req)
 	fmt.Printf("Path = %v.\n", req.Path)
 	fmt.Printf("method = %v.\n", req.HTTPMethod)
-	fmt.Printf("pathparam = %v.\n", req.QueryStringParameters["satellilte_name"])
+	fmt.Printf("QueryStringParameters = %v.\n", req.QueryStringParameters["satellite_name"])
+	fmt.Printf("pathparam = %v.\n", req.PathParameters["satellite_name"])
 	if "/topsecret" == req.Path {
 		return events.APIGatewayProxyResponse{Body: "topsecret", StatusCode: 200}, nil
-	} else if "/topsecret_split" == req.Path && "POST" == req.HTTPMethod && "" != req.QueryStringParameters["satellilte_name"] {
-		return events.APIGatewayProxyResponse{Body: "topsecret_split post" + req.QueryStringParameters["satellilte_name"], StatusCode: 200}, nil
+	} else if "/topsecret_split" == req.Path && "POST" == req.HTTPMethod && "" != req.QueryStringParameters["satellite_name"] {
+		return events.APIGatewayProxyResponse{Body: "topsecret_split post" + req.QueryStringParameters["satellite_name"], StatusCode: 200}, nil
 	} else if "/topsecret_split" == req.Path && "GET" == req.HTTPMethod {
 		return events.APIGatewayProxyResponse{Body: "topsecret_split get", StatusCode: 200}, nil
 	}
