db22cf9445ae1481525fdb9818c1778bc67ddccb
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-05 15:51:53 -0300

    path params

--- aws-stack/handler/top-secret-split/top-secret-split.go
+++ aws-stack/handler/top-secret-split/top-secret-split.go
@@ -9,28 +9,47 @@ import (
 )
 
 var topsecretSplitPath = "/topsecret_split"
+var topsecretPath = "/topsecret"
 
 func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	name := "Bastian"
-	fmt.Printf("hello: %s.\n", name)
+	//name := "Bastian"
+	//fmt.Printf("hello: %s.\n", name)
 	//greet, _ := GetMessages(name)
-	fmt.Printf("req = %v.\n", req)
-	fmt.Printf("Path = %v.\n", req.Path)
-	pathAux := strings.TrimSuffix(req.Path, "/")
+	//fmt.Printf("req = %v.\n", req)
+	//fmt.Printf("Path = %v.\n", req.Path)
+	//pathAux := strings.TrimSuffix(req.Path, "/")
+	pathAux := strings.Split(req.Path, "/")[1]
 	fmt.Printf("pathAux = %v.\n", pathAux)
 	fmt.Printf("method = %v.\n", req.HTTPMethod)
-	satelliteName := req.PathParameters["satellite_name"]
-	fmt.Printf("satellite_name pathparam = %v.\n", req.PathParameters["satellite_name"])
+	satelliteName := strings.ToLower(req.PathParameters["satellite_name"])
 	fmt.Printf("satelliteName = %v.\n", satelliteName)
-	if "/topsecret" == pathAux {
-		return events.APIGatewayProxyResponse{Body: "topsecret", StatusCode: 200}, nil
+
+	switch pathAux {
+	case topsecretPath:
+		fmt.Printf("redirect request to topsecret online api = %v.\n", satelliteName)
+		return events.APIGatewayProxyResponse{Body: "redirect request to topsecret online api", StatusCode: 200}, nil
+	case topsecretSplitPath:
+		if "GET" == req.HTTPMethod {
+			return events.APIGatewayProxyResponse{Body: "redirect request to get topsecret_split offline api" + satelliteName, StatusCode: 200}, nil
+		} else if "POST" == req.HTTPMethod && "" != satelliteName {
+			return events.APIGatewayProxyResponse{Body: "redirect request to post topsecret_split offline api" + satelliteName, StatusCode: 200}, nil
+		} else {
+			return events.APIGatewayProxyResponse{StatusCode: 404}, nil
+		}
+	default:
+		return events.APIGatewayProxyResponse{StatusCode: 404}, nil
+	}
+
+	/*if topsecretPath == pathAux {
+		fmt.Printf("redirect request to topsecret online api = %v.\n", satelliteName)
+		return events.APIGatewayProxyResponse{Body: "redirect request to topsecret online api", StatusCode: 200}, nil
 	} else if topsecretSplitPath == pathAux && "POST" == req.HTTPMethod && "" != satelliteName {
 		return events.APIGatewayProxyResponse{Body: "topsecret_split post" + satelliteName, StatusCode: 200}, nil
 	} else if topsecretSplitPath == pathAux && "GET" == req.HTTPMethod {
 		return events.APIGatewayProxyResponse{Body: "topsecret_split get", StatusCode: 200}, nil
-	}
+	}*/
 	fmt.Printf("router = %v.\n", req)
-	return events.APIGatewayProxyResponse{Body: name, StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{StatusCode: 404}, nil
 }
 
 func main() {
