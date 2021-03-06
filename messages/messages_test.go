275ce27d992095eb799b4439d5f43a2b7f957652
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-06 14:37:34 -0300

    clena code

--- messages/messages_test.go
+++ messages/messages_test.go
@@ -4,20 +4,21 @@ import (
 	"testing"
 )
 
-/*var kenobi_msg = [5]string{"este", "", "", "mensaje", ""}
-var skywalker_msg = [5]string{"", "es", "", "", "secreto"}
-var sato_msg = [5]string{"este", "", "un", "", ""}
-var kenobi_msg_6 = [6]string{"este", "", "", "mensaje", "", ""}*/
-var expectedMsg = "este es un mensaje secreto"
+const kMsg = [5]string{"este", "", "", "mensaje", ""}
+const skMsg = [5]string{"", "es", "", "", "secreto"}
+const saMsg = [5]string{"este", "", "un", "", ""}
+const kMsg_6 = [6]string{"este", "", "", "mensaje", "", ""}
+
+const exMsg = "este es un mensaje secreto"
 
 func TestMsgLen(t *testing.T) {
-	if valid, err := ValidateMessagesLen(kenobi_msg_6[:], skywalker_msg[:], sato_msg[:]); err == nil {
+	if valid, err := ValidateMessagesLen(kMsg_6[:], skMsg[:], saMsg[:]); err == nil {
 		t.Errorf("Expected: %v, got: %v", false, valid)
 		t.Errorf("Se esperaba ser invalido debido al largo de uno de los mensajes.")
 	} else {
 		t.Log("TestMsgLen PASSED")
 	}
-	if _, err := ValidateMessagesLen(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); err != nil {
+	if _, err := ValidateMessagesLen(kMsg[:], skMsg[:], saMsg[:]); err != nil {
 		t.Errorf("TestMsgLen FAILED: All messages were expected to have the same number of words.")
 	} else {
 		t.Log("TestMsgLen 2 PASSED")
@@ -25,8 +26,8 @@ func TestMsgLen(t *testing.T) {
 }
 
 func TestExpectedMsg(t *testing.T) {
-	if msg := GetMessage(kenobi_msg[:], skywalker_msg[:], sato_msg[:]); msg != expectedMsg {
-		t.Errorf("FAILED: Expected: %v, got: %v", expectedMsg, msg)
+	if msg := GetMessage(kMsg[:], skMsg[:], saMsg[:]); msg != exMsg {
+		t.Errorf("FAILED: Expected: %v, got: %v", exMsg, msg)
 	} else {
 		t.Log("TestExpectedMsg PASSED")
 	}
