b5d655bcf3e0890565c98e0573c51df41d846690
Bastias S, Bastian <bastian.bastias@consorcio.cl>
2021-03-06 21:46:12 -0300

    cognito integration

---
+++ aws-stack/handler/cognito/cognito.go
@@ -0,0 +1,18 @@
+package msgs
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("body:: %v \n", req.Body)
+	return events.APIGatewayProxyResponse{Body: "OK", StatusCode: 200}, nil
+}
+
+// the main function
+func main() {
+	lambda.Start(handleRequest)
+}
